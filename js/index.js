252e16c8664dfd46c6889ed3ed8a642b76065810
Erdal Budak <erdalbudak89@gmail.com>
2022-05-03 16:34:30 -0400

    commit tree 25325263181a8acd465df18a62ab09fad2f07de4

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {});
+
+const bookList = document.querySelector('#list');
+const details = document.querySelector("#show-panel");
+const user =  {id: 100001, username: "pouros"};
+
+document.addEventListener("DOMContentLoaded", function() {
+    // FETCH
+    fetch(`http://localhost:3000/books`)
+    .then(resp => resp.json())
+    .then(books => {
+        books.forEach((book) => {
+            bookHTML(book);
+        });
+    });
+});
+
+const bookHTML = (book) => {
+    const li = document.createElement('li');
+    li.addEventListener('click', () => {
+        details.innerHTML = "";
+        const img = document.createElement("img");
+        const title = document.createElement("h4");
+        const userList = document.createElement("ul");
+        const btn = document.createElement("button")
+        btn.innerText = "Like";
+        btn.addEventListener('click', () => {
+
+        })
+        
+        img.src = book.img_url;
+        title.innerText = book.title;
+        book.users.forEach((user) => {
+            const li = document.createElement('li')
+            li.innerText = user.username;
+            userList.append(li)
+        })
+        details.append(img, title, userList, btn);
+    });
+    li.innerText = book.title;
+    bookList.append(li);
+};
